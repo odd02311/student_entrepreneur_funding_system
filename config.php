3782ae6619559122a89f8130e8a333ed010c9b79
zhong zheng <danglu999@gmail.com>
2020-08-31 18:11:51 +0800

    add backend support for login

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    define('DB_HOST', '127.0.0.1:3306');
+    define('DB_DATABASE', 'student_entrepreneur_funding_system');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'admin123');
+    define('HOMEURL', 'index.php');
+?>
---
+++ login.php
@@ -0,0 +1,81 @@
+<?php
+	//Start session
+	session_start();
+	
+	//Include database connection details
+	require_once('config.php');
+	
+	//Array to store validation errors
+	$errmsg_arr = array();
+	
+	//Validation error flag
+	$errflag = false;
+	
+	//Connect to mysql server
+	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+	if(!$link) {
+		die('Failed to connect to server: ' . mysql_error());
+	}
+	
+	//Select database
+	$db = mysql_select_db(DB_DATABASE);
+	if(!$db) {
+		die("Unable to select database");
+	}
+	
+	//Function to sanitize values received from the form. Prevents SQL injection
+	function clean($str) {
+		$str = @trim($str);
+		if(get_magic_quotes_gpc()) {
+			$str = stripslashes($str);
+		}
+		return mysql_real_escape_string($str);
+	}
+	
+	//Sanitize the REQUEST values - parameters may come from GET or POST
+	$login = clean($_REQUEST['username_login']);
+	$password = clean($_REQUEST['password_login']);
+	
+	//Input Validations
+	if($login == '') {
+		$errmsg_arr[] = 'Username missing';
+		$errflag = true;
+	}
+	if($password == '') {
+		$errmsg_arr[] = 'Password missing';
+		$errflag = true;
+	}
+	
+	//If there are input validations, redirect back to the login form
+	if($errflag) {
+		$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
+		session_write_close();
+		header("location: login.php");
+		exit();
+	}
+	
+	//Create query
+	$qry="SELECT * FROM accounts WHERE username='$login' AND passcode='".md5($_REQUEST['password_login'])."'";
+	$result=mysql_query($qry);
+	
+	//Check whether the query was successful or not
+	if($result) {
+		if(mysql_num_rows($result) == 1) {
+			//Login Successful
+			session_regenerate_id();
+			$member = mysql_fetch_assoc($result);
+			$_SESSION['SESS_USERNAME'] = $member['username'];
+			session_write_close();
+			header("location: ".HOMEURL);
+			exit();
+		}else {
+			//Login failed
+			$errmsg_arr[] = 'Email or password wrong.';
+			$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
+			//header("location: login_form.php");
+			exit();
+		}
+	}else {
+		die("Query failed");
+	}
+?>
