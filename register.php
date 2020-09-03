f24b953788ae9f33bc5f7a31ddaaa4456ecf60ea
zhong zheng <danglu999@gmail.com>
2020-09-03 17:53:51 +0800

    registration PHP script

---
+++ register.php
@@ -0,0 +1,83 @@
+<?php
+	//Start session
+	session_start();
+	
+	//Include database connection details
+	require_once('config.php');
+
+	//Array to store validation errors
+	$errmsg_arr = array();
+	
+	//Validation error flag
+	$errflag = false;
+	
+	//Connect to mysql server
+	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+	if(!$link) {
+		die('Failed to connect to server: ' . mysql_error());
+	}
+	
+	//Select database
+	$db = mysql_select_db(DB_DATABASE);
+	if(!$db) {
+		die("Unable to select database");
+	}
+	
+	//Function to sanitize values received from the form. Prevents SQL injection
+	function clean($str) {
+		$str = @trim($str);
+		if(get_magic_quotes_gpc()) {
+			$str = stripslashes($str);
+		}
+		return mysql_real_escape_string($str);
+	}
+	
+	//Sanitize the POST values
+	$username = clean($_POST['username']);
+	$password = clean($_POST['password']);
+	$cpassword = clean($_POST['password_confirm']);
+
+	if($username == '') {
+		$errmsg_arr[] = 'Username missing';
+		$errflag = true;
+	}
+
+	if($password == '') {
+		$errmsg_arr[] = 'Password missing';
+		$errflag = true;
+	}
+	if($cpassword == '') {
+		$errmsg_arr[] = 'Confirm password missing';
+		$errflag = true;
+	}
+	if( strcmp($password, $cpassword) != 0 ) {
+		$errmsg_arr[] = 'Passwords do not match';
+		$errflag = true;
+	}
+	
+	//Check for duplicate email address
+	if($email != '') {
+		$qry = "SELECT * FROM members WHERE email='$email'";
+
+		$result = mysql_query($qry);
+		if($result) {
+			if(mysql_num_rows($result) > 0) {
+				$errmsg_arr[] = 'Email already in use';
+				$errflag = true;
+			}
+			@mysql_free_result($result);
+		}
+		else {
+			die("Query failed");
+		}
+	}
+	
+	//If there are input validations, redirect back to the registration form
+	if($errflag) {
+		$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
+		session_write_close();
+		header("location: error.php");
+		exit();
+	}
+
+?>
