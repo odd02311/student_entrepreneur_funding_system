7724dc5f626ff58359780a58bc85e226acf56337
zhong zheng <danglu999@gmail.com>
2020-09-09 12:49:45 +0800

    update config info

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
     define('DB_HOST', '127.0.0.1:3306');
     define('DB_DATABASE', 'student_entrepreneur_funding_system');
     define('DB_USER', 'root');
-    define('DB_PASSWORD', 'admin123');
+    define('DB_PASSWORD', 'ewqewq');
     define('HOMEURL', 'index.php');
 ?>
--- login.php
+++ login.php
@@ -10,13 +10,14 @@
 	
 	//Validation error flag
 	$errflag = false;
-	
+
 	//Connect to mysql server
 	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 	if(!$link) {
 		die('Failed to connect to server: ' . mysql_error());
 	}
 	
+
 	//Select database
 	$db = mysql_select_db(DB_DATABASE);
 	if(!$db) {
