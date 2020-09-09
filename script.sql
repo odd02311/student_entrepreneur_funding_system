bc4822d1d96ff33a8ec24902a89ad8e1a57c2bf1
zhong zheng <danglu999@gmail.com>
2020-09-09 11:35:25 +0800

    updated database script

--- script.sql
+++ script.sql
@@ -2,10 +2,15 @@ DROP DATABASE IF EXISTS `students_funding_system`;
 CREATE DATABASE IF NOT EXISTS `students_funding_system` DEFAULT CHARACTER SET 'UTF8';
 USE `students_funding_system`;
 
+DROP table IF EXISTS accounts;
+DROP table IF EXISTS productions;
+DROP table IF EXISTS comments;
+DROP table IF EXISTS operations;
+DROP table IF EXISTS  operation_types;
 
 CREATE TABLE IF NOT EXISTS accounts(
 user_id INT UNSIGNED AUTO_INCREMENT KEY,
-username VARCHAR(30) NOT NULL UNIQUE,
+username VARCHAR(40) NOT NULL,
 password VARCHAR(100) NOT NULL,
 headimg_url VARCHAR(255) NOT NULL
 );
@@ -23,15 +28,15 @@ url VARCHAR(255) NOT NULL
 
 CREATE TABLE IF NOT EXISTS comments(
 id INT UNSIGNED AUTO_INCREMENT KEY,
-product_id UNSIGNED NOT NULL,
+product_id INT UNSIGNED NOT NULL REFERENCES productions(product_id),
 user_id INT UNSIGNED NOT NULL,
 content VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS operations(
 oper_id INT UNSIGNED AUTO_INCREMENT KEY,
-user_id UNSIGNED NOT NULL,
-type_id UNSIGNED NOT NULL
+user_id INT UNSIGNED NOT NULL REFERENCES accounts(user_id),
+type_id INT UNSIGNED NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS operation_types(
