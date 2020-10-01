7818767f2d1a52bc067e9f804be1f81653315e43
zhong zheng <danglu999@gmail.com>
2020-10-01 16:34:51 +0800

    test data inserted 1

--- script.sql
+++ script.sql
@@ -30,9 +30,9 @@ category VARCHAR(50) NOT NULL,
 pic_url VARCHAR(255) NOT NULL,
 video_url VARCHAR(255) NOT NULL,
 author VARCHAR(50) NOT NULL,
-views INT UNSIGNED NOT NULL,
-likes INT UNSIGNED NOT NULL,
-dislikes INT UNSIGNED NOT NULL,
+views INT UNSIGNED DEFAULT 0,
+likes INT UNSIGNED DEFAULT 0,
+dislikes INT UNSIGNED DEFAULT 0,
 create_date DATETIME DEFAULT CURRENT_TIMESTAMP()
 );
 
@@ -56,3 +56,23 @@ CREATE TABLE IF NOT EXISTS operation_types(
 type_id INT UNSIGNED NOT NULL,
 description VARCHAR(30) NOT NULL
 );
+
+
+insert into accounts (username, password, is_admin, email, phone, create_date)
+values
+('kelvin', 'e10adc3949ba59abbe56e057f20f883e', 0, 'kelvin@gmail.com', '+65 77889933', STR_TO_DATE('30,9,2020','%d,%m,%Y')),
+('test_username', 'e10adc3949ba59abbe56e057f20f883e', 0, 'test_username@gmail.com', '+65 432435443', STR_TO_DATE('30,5,2020','%d,%m,%Y'))
+
+
+
+insert into productions (username, author, category, title, description, pic_url, video_url, create_date)
+values
+('kelvin',  'ChilledCow', 'Video', 'lofi hip hop radio', 'lofi hip hop radio - beats to relax/study to
+',  'https://resources.matcha-jp.com/resize/720x2000/2020/04/23-101958.jpeg', 'https://www.youtube.com/embed/pvPsJFRGleA' , STR_TO_DATE('30,5,2020','%d,%m,%Y')),
+
+('kelvin',  'ChilledCow', 'Video', 'lofi hip hop radio', 'lofi hip hop radio - beats to relax/study to
+',  'https://resources.matcha-jp.com/resize/720x2000/2020/04/23-101958.jpeg', 'https://www.youtube.com/embed/pvPsJFRGleA' , STR_TO_DATE('30,5,2020','%d,%m,%Y')),
+
+('Anne', 'WLOP', 'Video', 'Photoshop painting process - Jewel', 
+'If you want to get the high res wallpaper images and full painting process video, come to my patreon: https://www.patreon.com/wlop.', 
+'https://img.youtube.com/vi/X9IkwoMrO9w/maxresdefault.jpg', 'https://www.youtube.com/watch?v=X9IkwoMrO9w&t=93s', STR_TO_DATE('7,5,2020','%d,%m,%Y'))
